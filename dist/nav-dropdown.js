d384cc3a172cb85ae322b015d61965e9a6a5398d
Naoki Sekiguchi <naokis@ranagram.com>
2014-08-22 18:56:43 +0900

    Renamed objectname from "$.DropdownNav" to "$.NavDropdown"

--- dist/nav-dropdown.js
+++ dist/nav-dropdown.js
@@ -1,98 +1,108 @@
-'use strict';
-$.DropdownNav = Backbone.View.extend({
-  el: 'li',
-  initialize: function(options) {
-    this.opt = {
-      transitionDuration: 0,
-      child: 'ul'
-    };
-    $.extend(this.opt, options);
-    _.bindAll(this, 'update', 'handler', 'closeAll');
-    setTimeout(this.update, 500);
-    $(window).on('resize orientationchange', _.debounce(this.update, 500));
-    this.opened = false;
-  },
-  update: function() {
-    var errorMsg, self;
-    self = this;
-    errorMsg = '$.DropdownNav: child element not found';
-    return this.$el.each(function(i, el) {
-      var $child, $el, $link;
-      $el = $(el);
-      $link = $el.find('>a');
-      if ($link[0]) {
-        $link.data('container', el);
-        $link.on('click', self.handler);
-      } else {
-        $el.on('click', self.handler);
-      }
-      if (self.opt.child === null) {
+
+/*
+ * $.NavDropdown
+ *
+ * @author     Naoki Sekiguchi
+ * @license    MIT
+ * @require    jquery.js, underscore.js, backbone.js
+ */
+(function(Backbone, _, $, window) {
+  'use strict';
+  $.NavDropdown = Backbone.View.extend({
+    el: 'li',
+    initialize: function(options) {
+      this.opt = {
+        transitionDuration: 0,
+        child: 'ul'
+      };
+      $.extend(this.opt, options);
+      _.bindAll(this, 'update', 'handler', 'closeAll');
+      setTimeout(this.update, 500);
+      $(window).on('resize orientationchange', _.debounce(this.update, 500));
+      this.opened = false;
+    },
+    update: function() {
+      var errorMsg, self;
+      self = this;
+      errorMsg = '$.NavDropdown: child element not found';
+      return this.$el.each(function(i, el) {
+        var $child, $el, $link;
+        $el = $(el);
+        $link = $el.find('>a');
         if ($link[0]) {
-          $child = $($link.attr('href'));
-          if ($child[0] == null) {
+          $link.data('container', el);
+          $link.on('click', self.handler);
+        } else {
+          $el.on('click', self.handler);
+        }
+        if (self.opt.child === null) {
+          if ($link[0]) {
+            $child = $($link.attr('href'));
+            if ($child[0] == null) {
+              return;
+            }
+          } else {
+            console.error(errorMsg);
             return;
           }
         } else {
-          console.error(errorMsg);
-          return;
+          $child = $el.find(self.opt.child);
         }
-      } else {
-        $child = $el.find(self.opt.child);
-      }
-      if ($child[0] != null) {
-        $child.css({
-          height: '',
-          visibility: 'hidden'
-        });
-        $el.data('childheight', $child.height());
-        $child.css({
-          height: 0,
-          visibility: 'visible'
-        });
-        $el.data('$child', $child);
-      }
-    });
-  },
-  handler: function(e) {
-    var $child, $trigger, trigger;
-    trigger = e.currentTarget;
-    if ($(trigger).data('container')) {
-      trigger = $(trigger).data('container');
-    }
-    $trigger = $(trigger);
-    $child = $trigger.data('$child');
-    if ($child == null) {
-      this.closeAll(trigger);
-      this.opened = false;
-      return true;
-    }
-    this.closeAll(trigger).done(function() {
-      if ($child.hasClass('on')) {
-        return $child.removeClass('on').height(0);
-      } else {
-        return $child.addClass('on').height($trigger.data('childheight'));
-      }
-    });
-    this.opened = true;
-    e.preventDefault();
-  },
-  closeAll: function(exclude) {
-    var dfd;
-    dfd = $.Deferred();
-    this.$el.each(function(i, el) {
-      var $child;
-      $child = $(el).data('$child');
-      if (el !== exclude) {
-        if (($child != null) && $child[0] && $child.hasClass('on')) {
-          $child.removeClass('on').height(0);
+        if ($child[0] != null) {
+          $child.css({
+            height: '',
+            visibility: 'hidden'
+          });
+          $el.data('childheight', $child.height());
+          $child.css({
+            height: 0,
+            visibility: 'visible'
+          });
+          $el.data('$child', $child);
         }
+      });
+    },
+    handler: function(e) {
+      var $child, $trigger, trigger;
+      trigger = e.currentTarget;
+      if ($(trigger).data('container')) {
+        trigger = $(trigger).data('container');
       }
-    });
-    if (this.opened === true) {
-      setTimeout(dfd.resolve, this.opt.transitionDuration);
-    } else {
-      dfd.resolve();
+      $trigger = $(trigger);
+      $child = $trigger.data('$child');
+      if ($child == null) {
+        this.closeAll(trigger);
+        this.opened = false;
+        return true;
+      }
+      this.closeAll(trigger).done(function() {
+        if ($child.hasClass('on')) {
+          return $child.removeClass('on').height(0);
+        } else {
+          return $child.addClass('on').height($trigger.data('childheight'));
+        }
+      });
+      this.opened = true;
+      e.preventDefault();
+    },
+    closeAll: function(exclude) {
+      var dfd;
+      dfd = $.Deferred();
+      this.$el.each(function(i, el) {
+        var $child;
+        $child = $(el).data('$child');
+        if (el !== exclude) {
+          if (($child != null) && $child[0] && $child.hasClass('on')) {
+            $child.removeClass('on').height(0);
+          }
+        }
+      });
+      if (this.opened === true) {
+        setTimeout(dfd.resolve, this.opt.transitionDuration);
+      } else {
+        dfd.resolve();
+      }
+      return dfd.promise();
     }
-    return dfd.promise();
-  }
-});
+  });
+})(Backbone, _, jQuery, window);
--- src/nav-dropdown.coffee
+++ src/nav-dropdown.coffee
@@ -1,86 +1,99 @@
-'use strict'
-$.DropdownNav = Backbone.View.extend(
-  el: 'li'
-  initialize: (options) ->
-    @opt = {
-      transitionDuration: 0
-      child: 'ul'
-    }
-    $.extend(@opt, options)
-    _.bindAll(this, 'update', 'handler', 'closeAll')
-    setTimeout @update, 500
-    $(window).on 'resize orientationchange', _.debounce(@update, 500)
-    @opened = false
-    return
+###
+ * $.NavDropdown
+ *
+ * @author     Naoki Sekiguchi
+ * @license    MIT
+ * @require    jquery.js, underscore.js, backbone.js
+###
 
-  update: ->
-    self = @
-    errorMsg = '$.DropdownNav: child element not found'
-    @$el.each (i, el) ->
-      $el = $(el)
-      $link = $el.find('>a')
-      if $link[0]
-        $link.data 'container', el
-        $link.on 'click', self.handler
-      else
-        $el.on 'click', self.handler
+((Backbone, _, $, window) ->
+  'use strict'
 
-      if self.opt.child is null
-        if $link[0]
-          $child = $($link.attr('href'))
-          if !$child[0]?
-            return # nothing to do
-        else
-          console.error errorMsg
-          return
-      else
-        $child = $el.find(self.opt.child)
-
-      if $child[0]?
-        $child.css
-          height: ''
-          visibility: 'hidden'
-        $el.data('childheight', $child.height()) # save height
-        $child.css
-          height: 0
-          visibility: 'visible'
-        $el.data('$child', $child)
-      return
-
-  handler: (e) ->
-    trigger = e.currentTarget
-    if $(trigger).data('container')
-      trigger = $(trigger).data('container')
-    $trigger = $(trigger)
-    $child = $trigger.data('$child')
-    if !$child?
-      @closeAll(trigger)
+  $.NavDropdown = Backbone.View.extend(
+    el: 'li'
+    initialize: (options) ->
+      @opt = {
+        transitionDuration: 0
+        child: 'ul'
+      }
+      $.extend(@opt, options)
+      _.bindAll(this, 'update', 'handler', 'closeAll')
+      setTimeout @update, 500
+      $(window).on 'resize orientationchange', _.debounce(@update, 500)
       @opened = false
-      return true # default link
-
-    @closeAll(trigger).done ->
-      if $child.hasClass('on')
-        # off
-        $child.removeClass('on').height(0)
-      else
-        # on
-        $child.addClass('on')
-          .height($trigger.data('childheight'))
-    @opened = true
-    e.preventDefault()
-    return
-
-  closeAll: (exclude) ->
-    dfd = $.Deferred()
-    @$el.each (i, el) ->
-      $child = $(el).data('$child')
-      if el isnt exclude
-        if $child? and $child[0] and $child.hasClass('on')
-          $child.removeClass('on').height(0)
       return
-    if @opened is true
-      setTimeout dfd.resolve, @opt.transitionDuration
-    else
-      dfd.resolve()
-    dfd.promise()
-)
+
+    update: ->
+      self = @
+      errorMsg = '$.NavDropdown: child element not found'
+      @$el.each (i, el) ->
+        $el = $(el)
+        $link = $el.find('>a')
+        if $link[0]
+          $link.data 'container', el
+          $link.on 'click', self.handler
+        else
+          $el.on 'click', self.handler
+
+        if self.opt.child is null
+          if $link[0]
+            $child = $($link.attr('href'))
+            if !$child[0]?
+              return # nothing to do
+          else
+            console.error errorMsg
+            return
+        else
+          $child = $el.find(self.opt.child)
+
+        if $child[0]?
+          $child.css
+            height: ''
+            visibility: 'hidden'
+          $el.data('childheight', $child.height()) # save height
+          $child.css
+            height: 0
+            visibility: 'visible'
+          $el.data('$child', $child)
+        return
+
+    handler: (e) ->
+      trigger = e.currentTarget
+      if $(trigger).data('container')
+        trigger = $(trigger).data('container')
+      $trigger = $(trigger)
+      $child = $trigger.data('$child')
+      if !$child?
+        @closeAll(trigger)
+        @opened = false
+        return true # default link
+
+      @closeAll(trigger).done ->
+        if $child.hasClass('on')
+          # off
+          $child.removeClass('on').height(0)
+        else
+          # on
+          $child.addClass('on')
+            .height($trigger.data('childheight'))
+      @opened = true
+      e.preventDefault()
+      return
+
+    closeAll: (exclude) ->
+      dfd = $.Deferred()
+      @$el.each (i, el) ->
+        $child = $(el).data('$child')
+        if el isnt exclude
+          if $child? and $child[0] and $child.hasClass('on')
+            $child.removeClass('on').height(0)
+        return
+      if @opened is true
+        setTimeout dfd.resolve, @opt.transitionDuration
+      else
+        dfd.resolve()
+      dfd.promise()
+  )
+
+  return
+)(Backbone, _, jQuery, window)
